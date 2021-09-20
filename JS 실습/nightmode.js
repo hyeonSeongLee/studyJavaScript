c2a3e87a27565101e128dd55a02b691d877e7c8f
HyeonseongLee <b01099834850@gmail.com>
2021-09-20 22:34:48 +0900

    JS 실습 중

---
+++ JS 실습/nightmode.js
@@ -0,0 +1,39 @@
+var body = {
+    setColor:function (inputColor) {
+    var target = document.querySelector('body')
+    target.style.color=inputColor
+    },
+    setBackgroundColor:function (inputColor) {
+    var target = document.querySelector('body')
+    target.style.backgroundColor=inputColor
+    }
+
+}
+var link = {
+    setColor: function (inputColor) {
+    var alist = document.querySelectorAll('a')
+    var i = 0
+    while (i<alist.length) {
+    alist[i].style.color=inputColor
+    i=i+1 }  
+    }
+}
+function nightDayHandler (self) {
+var target = document.querySelector('body')
+if(self.value === 'night')
+{
+    body.setBackgroundColor('black')
+    body.setColor('aquamarine')
+    document.querySelector('#night-day').value= 'day'
+    
+    link.setColor('aquamarine')
+}
+else
+{
+    body.setBackgroundColor('white')
+    body.setColor ('teal')
+    document.querySelector('#night-day').value= 'night'
+
+    link.setColor('teal')
+}
+}
