dfc0b96ee9ce213bed7994af7ded4744489fc501
HyeonseongLee <b01099834850@gmail.com>
2021-09-23 23:41:29 +0900

    JS 복습 중

--- JS 실습/final_night.js
+++ JS 실습/final_night.js
@@ -1,3 +1,18 @@
-body.backgroundcolor(color) = function(color){
-    document.querySelector('body').backgroundcolor(color)
+function bodyBackgroundColor(color){
+    var target = document.querySelector('body')
+    target.style.backgroundColor=color
+}
+
+function bodyColor(color){
+    var target = document.querySelector('body')
+    target.style.color=color
+}
+
+function linkColor(color){
+    var link = document.querySelectorAll('a')
+    var i = 0
+    while (i<link.length) {
+        link[i].style.color=color
+        i = i+1
+    }
 }
